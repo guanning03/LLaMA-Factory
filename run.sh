bc8016456f60dd7fa0543f3b991c0ccb5abc539d
guanning03 <zgn21@mails.tsinghua.edu.cn>
2025-04-15 15:59:49 +0800

    adapt to H100

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 llamafactory-cli train trial/qwen2.5-3b-instruct_ts.yaml
+CUDA_VISIBLE_DEVICES=4,5 llamafactory-cli train trial/qwen2.5-3b-instruct_ts.yaml
+modelscope download --model Qwen/Qwen2.5-3B-Instruct --local_dir models/Qwen2.5-3B-Instruct
